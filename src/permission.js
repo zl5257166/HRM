a2d8a3c09a289f0c5c3d8e98e4874b2878a167e7
钟良 <zhongliang@nistone.com>
2021-06-01 10:11:13 +0800

    [feat]:主页token拦截处理

---
+++ src/permission.js
@@ -0,0 +1,31 @@
+import router from '@/router'
+import store from '@/store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+const whiteList = ['/login', '/404']
+
+// 路由前置守卫
+
+router.beforeEach(function(to, from, next) {
+  NProgress.start() // 开启进度条
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  NProgress.done()
+})
+
+// 后置守卫
+router.afterEach(function() {
+  NProgress.done() // 关闭进度条
+})
