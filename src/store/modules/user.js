1786377a30dc7a17bed1a2f23f034c8b8060ec59
钟良 <zhongliang@nistone.com>
2021-06-01 13:57:29 +0800

    【chore】:封装获取用户资料的action并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,25 +1,55 @@
-import { login } from '@/api/user'
+import { login, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 
 const state = {
-  token: getToken()
+  token: getToken(),
+  userInfo: {}
 }
 
 const mutations = {
+  /**
+   * 设置token
+   */
   SET_TOKEN(state, token) {
     state.token = token
     setToken(token)
   },
+  /**
+   * 移除token
+   */
   REMOVE_TOKEN(state) {
     state.token = null
     removeToken()
+  },
+  /**
+   * 设置userInfo
+   */
+  SET_USERINFO(state, userInfo) {
+    state.userInfo = { ...userInfo }
+  },
+  /**
+   * 删除userInfo
+   */
+  REMOVE_USERINFO(state) {
+    state.userInfo = {}
   }
 }
 
 const actions = {
+  /**
+   * 登录
+   */
   async login(context, data) {
     const result = await login(data) // 拿到 token
     context.commit('SET_TOKEN', result) // 缓存 token
+  },
+  /**
+   * 获取用户资料
+   */
+  async getUserInfo(context) {
+    const result = await getInfo()
+    context.commit('SET_USERINFO', result)
+    return result
   }
 }
 
