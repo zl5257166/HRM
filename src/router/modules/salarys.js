92992650c4a2dc3136672fb76e813be5d9b757b3
钟良 <zhongliang@nistone.com>
2021-06-02 10:16:12 +0800

    【chore】:业务模块页面的快速搭建（新建页面，设置每个模块路由规则）

---
+++ src/router/modules/salarys.js
@@ -0,0 +1,16 @@
+import Layout from '@/layout'
+
+export default {
+  path: '/salarys',
+  name: 'salarys',
+  component: Layout,
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/salarys'),
+      meta: {
+        title: '工资'
+      }
+    }
+  ]
+}
