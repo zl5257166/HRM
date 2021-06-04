e5c3e7d1efb3d217146dc5dd8ac4ba5e8631ec5f
钟良 <zhongliang@nistone.com>
2021-06-04 14:18:28 +0800

    【feat】:将树形内容单独抽提组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,51 +3,9 @@
     <div class="app-container">
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span>
-                    操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-item :t-node="company" :is-root="true" />
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height:40px;width:100%">
-            <el-col>
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <el-dropdown>
-                    <span>操作<i class="el-icon-arrow-down" /></span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除子部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-item slot-scope="{ data }" :t-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -55,7 +13,9 @@
 </template>
 
 <script>
+import treeItem from './components/treeItem.vue'
 export default {
+  components: { treeItem },
   data() {
     return {
       departs: [
@@ -80,6 +40,10 @@ export default {
       ],
       defaultProps: {
         label: 'name'
+      },
+      company: {
+        name: '有梦想就想办法实现科技有限公司',
+        manager: '负责人'
       }
     }
   }
